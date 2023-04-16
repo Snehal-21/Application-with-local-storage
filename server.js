ac0aa54debbf481663184565f1b8d0e54b9d0363
Snehal Navale <navalesnehal21@gmail.com>
2023-04-16 19:39:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,47 +1,113 @@
-function register(event){
+function register(event) {
     event.preventDefault();
-    var name=document.getElementById("username").value;
+    var name = document.getElementById("username").value;
     // console.log(name);
-    var email=document.getElementById("useremail").value;
+    var email = document.getElementById("useremail").value;
     // console.log(email);
-    var password=document.getElementById("userpassword").value;
+    var password = document.getElementById("userpassword").value;
     // console.log(password);
-    var confirmpassword=document.getElementById("userconfirmpassword").value;
+    var confirmpassword = document.getElementById("userconfirmpassword").value;
     // console.log(confirmpassword);
+
+
+    
+//     var dataarray = JSON.parse(localStorage.getItem("users")) || [];
+//     var flag = false;
+//     for ( i = 0; i < dataarray.length ; i++) {
+//         // console.log(dataarray[i],i)
+//         console.log(email, "entered email");
+//         console.log(dataarray[i].uemail, "stored emails")
+//         if (dataarray[i].uemail == email) {
+//             flag = true;
+//         }
+//     }
+//     if (flag == false) {
+//         var userdata = { uname: name, uemail: email, upass: password, uconfirmpass: confirmpassword }
+//         dataarray.push(userdata)
+//         localStorage.setItem("users", JSON.stringify(dataarray))
+//         alert("registered successful");
+//         document.getElementById("username").value = ''
+//         document.getElementById("useremail").value = ''
+//         document.getElementById("userpassword").value = ''
+//         document.getElementById("userconfirmpassword").value = ''
+//     }
+//     else {
+//         alert("use unique Email..")
+//     }
+
+
+
+//     //addition
+
+// }
+
+    
+
 
 
     if(name && email && password && confirmpassword){
         if(password.length >=8 && confirmpassword.length >=8){
             if(password==confirmpassword){
-                var userdata={uname:name, uemail:email,upass:password,uconfirmpass:confirmpassword}//creation of object syntax:var objectname={key:value}
-                console.log(userdata)
-
-                localStorage.setItem("users",JSON.stringify(userdata))
-            }
-            else{
-                console.log("password not matched");
-            }
+    var dataarray = JSON.parse(localStorage.getItem("users")) || [];
+    var flag = false;
+    for ( i = 0; i < dataarray.length ; i++) {
+        // console.log(dataarray[i],i)
+        console.log(email, "entered email");
+        console.log(dataarray[i].uemail, "stored emails")
+        if (dataarray[i].uemail == email) {
+            flag = true;
         }
+    }
+    if (flag == false) {
+        var userdata = { uname: name, uemail: email, upass: password, uconfirmpass: confirmpassword }
+        dataarray.push(userdata)
+        localStorage.setItem("users", JSON.stringify(dataarray))
+        alert("registered successful");
+        document.getElementById("username").value = ''
+        document.getElementById("useremail").value = ''
+        document.getElementById("userpassword").value = ''
+        document.getElementById("userconfirmpassword").value = ''
+    }
+    else {
+        alert("use unique Email..")
+    }   
+        }
+        else{
+            console.log("password not matched");
+        }
+    }
         else {
             console.log("password should be minimum 8 digits")
         }
     }
+
     else{
         console.log("all fields are required")
     }
-}
-function getdata(){
-    var data= JSON.parse(localStorage.getItem("users"))
-    console.log("fetched data",data)
+
 }
 
 
+// function getdata(){
+//     var data= JSON.parse(localStorage.getItem("users"))
+//     console.log("fetched data",data)
+// }
+
+// const arraydata=[];
+// function multiuser(event){
+//     event.preventDefault();
+//     var name=document.getElementById("username").value;
+//     var email=document.getElementById("useremail").value;
+//     var password=document.getElementById("userpassword").value;
+//     var confirmpassword=document.getElementById("userconfirmpassword").value;
+
+//     var userdata={uname:name, uemail:email,upass:password,uconfirmpass:confirmpassword}
+//     arraydata.push(userdata);
+//     localStorage.setItem("user",JSON.stringify(arraydata))
+
+
+// }
 
 
 
 
-// JSON.stringify //conert obgect into json string
-// JSON.parse //convrt json into object
-// localStorage.setItem(key,value) //use to store data into local storage
-// localStorage.getItem(key) //to get data from local storage
-// localStorage.removeItem(key) //to delete data from local storage
